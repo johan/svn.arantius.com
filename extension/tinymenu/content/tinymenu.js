e21af4b5f9858e79bdc9913792285bfd7397c77c
Anthony Lieuallen <arantius@gmail.com>
2006-04-28 03:43:26 +0000

    configure which menus go tiny
    git-svn-id: http://svn.arantius.com@79 28f79fa8-d1f5-0310-81f3-f5ca3e6eca6b

--- extension/tinymenu/content/tinymenu.js
+++ extension/tinymenu/content/tinymenu.js
@@ -1,19 +1,80 @@
-window.addEventListener('load', function(){
-//find the main menu
-var menubar=document.getElementById('main-menubar');
-//find our menu popup
-var menusub=document.getElementById('tinymenu-popup');
+var tinymenu={
 
-//move each of the menus into the sub menu
-var el;
-while (el=menubar.childNodes[0]) {
-	if ('tinymenu'==el.id) break;
+menuIds:[
+	'file-menu', 'edit-menu', 'view-menu', 'go-menu',
+	'bookmarks-menu', 'tools-menu', 'helpMenu'
+],
+doNotCollapse:'',
 
-	menubar.removeChild(el);
-	menusub.appendChild(el);
+initPref:function() {
+	var prefService=Components.classes['@mozilla.org/preferences-service;1']
+		.getService(Components.interfaces.nsIPrefBranch);
+	tinymenu.doNotCollapse=prefService.getCharPref('tinymenu.doNotCollapse');
+},
+
+onLoad:function() {
+	tinymenu.initPref();
+
+	// don't execute in options window
+	if ('undefined'==typeof gBrowser) return;
+
+	//find the main menu
+	var menubar=document.getElementById('main-menubar');
+	//find our menu popup
+	var menusub=document.getElementById('tinymenu-popup');
+
+	//move each of the menus into the sub menu
+	var el, r;
+	//while (el=menubar.childNodes[0]) {
+	for (var i=menubar.childNodes.length-1; i>=0; i--) {
+		el=menubar.childNodes[i];
+		
+		r=new RegExp('\\b'+el.id+'\\b');
+		if (r.exec(tinymenu.doNotCollapse)) continue;
+
+		menubar.removeChild(el);
+		menusub.insertBefore(el, menusub.firstChild);
+	}
+
+	//put the new items in our menu popup
+	var menupop=document.getElementById('tinymenu');
+	menupop.appendChild(menusub);
+},
+
+loadOptions:function() {
+	tinymenu.initPref();
+
+	var r, id;
+	for (var i in tinymenu.menuIds) {
+		id=tinymenu.menuIds[i];
+
+		r=new RegExp('\\b'+id+'\\b');
+		document.getElementById('pref-'+id).checked=
+			r.exec(tinymenu.doNotCollapse);
+	}
+},
+
+saveOptions:function() {
+	var opt='tinymenu';
+
+	var r, id;
+	for (var i in tinymenu.menuIds) {
+		id=tinymenu.menuIds[i];
+
+		if (document.getElementById('pref-'+id).checked) {
+			opt+=' '+id;
+		}
+	}
+	
+	if (tinymenu.doNotCollapse!=opt) {
+		tinymenu.doNotCollapse=opt;
+
+		var prefService=Components.classes['@mozilla.org/preferences-service;1']
+			.getService(Components.interfaces.nsIPrefBranch);
+		prefService.setCharPref('tinymenu.doNotCollapse', opt);
+	}
 }
 
-//put the new items in our menu popup
-var menupop=document.getElementById('tinymenu');
-menupop.appendChild(menusub);
-}, false);
+}//end var tinymenu
+
+window.addEventListener('load', tinymenu.onLoad, false);
