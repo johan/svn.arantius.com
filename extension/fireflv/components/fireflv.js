6d556ec1c8ae9b29ae592c40e59517be55327860
Anthony Lieuallen <arantius@gmail.com>
2007-07-15 19:44:51 +0000

    Add initial testing version of "FireFlash Video" extension.
    git-svn-id: http://svn.arantius.com@320 28f79fa8-d1f5-0310-81f3-f5ca3e6eca6b

---
+++ extension/fireflv/components/fireflv.js
@@ -0,0 +1,76 @@
+//const FIREFLV_CONTRACTID='@arantius.com/fireflv-channel-manager;1';
+//const FIREFLV_CID=Components.ID('{cebd8118-e3c9-4d81-abd8-538c8b7abed5}');
+
+const FIREFLV_POLICY_CONTRACTID='@arantius.com/fireflv-policy;1';
+const FIREFLV_POLICY_CID=Components.ID('{cebd8118-e3c9-4d81-abd8-538c8b7abed5}');
+
+const CATMAN_CONTRACTID='@mozilla.org/categorymanager;1';
+const JSLOADER_CONTRACTID='@mozilla.org/moz/jssubscript-loader;1';
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+
+var Module={
+	factoryLoaded:false,
+
+	registerSelf:function(compMgr, fileSpec, location, type) {
+		compMgr=compMgr.QueryInterface(
+			Components.interfaces.nsIComponentRegistrar
+		);
+
+		compMgr.registerFactoryLocation(
+			FIREFLV_POLICY_CID, 'FireFlash Video Content Policy', FIREFLV_POLICY_CONTRACTID,
+			fileSpec, location, type
+		);
+		var catman=Components.classes[CATMAN_CONTRACTID]
+			.getService(Components.interfaces.nsICategoryManager);
+		catman.addCategoryEntry(
+			'content-policy', FIREFLV_POLICY_CONTRACTID, FIREFLV_POLICY_CONTRACTID, true, true
+		);
+	},
+
+	unregisterSelf:function(compMgr, fileSpec, location) {
+		compMgr=compMgr.QueryInterface(
+			Components.interfaces.nsIComponentRegistrar
+		);
+
+		compMgr.unregisterFactoryLocation(FIREFLV_POLICY_CID, fileSpec);
+
+		var catman=Components.classes[CATMAN_CONTRACTID]
+			.getService(Components.interfaces.nsICategoryManager);
+
+		catman.deleteCategoryEntry('content-policy', FIREFLV_POLICY_CONTRACTID, true);
+	},
+
+	getClassObject:function(compMgr, cid, iid) {
+		if (!cid.equals(FIREFLV_POLICY_CID)) {
+			throw Components.results.NS_ERROR_NO_INTERFACE;
+		}
+
+		if (!iid.equals(Components.interfaces.nsIFactory)) {
+			throw Components.results.NS_ERROR_NOT_IMPLEMENTED;
+		}
+
+		if (!this.factoryLoaded) {
+			var loader=Components.classes[JSLOADER_CONTRACTID]
+				.getService(Components.interfaces.mozIJSSubScriptLoader);
+
+			loader.loadSubScript('chrome://fireflv/content/fireflv-policy.js');
+			//loader.loadSubScript('chrome://fireflv/content/fireflv-parse.js');
+
+			this.factoryLoaded=true;
+		}
+
+		return gFireFlvPolicyFactory;
+	},
+
+	canUnload:function(compMgr) {
+		return true;
+	}
+};
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+
+// module initialisation
+function NSGetModule(comMgr, fileSpec) {
+	return Module;
+}
---
+++ extension/fireflv/content/fireflv-policy.js
@@ -0,0 +1,75 @@
+var gFireFlvPolicy={
+	ACCEPT:Components.interfaces.nsIContentPolicy.ACCEPT,
+	REJECT:Components.interfaces.nsIContentPolicy.REJECT_REQUEST,
+
+	// nsIContentPolicy interface implementation
+	shouldLoad:function(
+		contentType, contentLocation, requestOrigin, requestingNode, mimeTypeGuess, extra
+	) {
+		return this.ACCEPT;
+	},
+
+	// nsISupports interface implementation
+	shouldProcess:function(
+		contentType, contentLocation, requestOrigin, requestingNode, mimeType, extra
+	) {
+		return this.ACCEPT;
+	},
+
+	// nsISupports interface implementation
+	QueryInterface:function(iid) {
+		if (!iid.equals(Components.interfaces.nsISupports) &&
+			!iid.equals(Components.interfaces.nsIContentPolicy)
+		) {
+			throw Components.results.NS_ERROR_NO_INTERFACE;
+		}
+
+		return this;
+	}
+};
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+
+// Factory object
+var gFireFlvPolicyFactory={
+	// nsIFactory interface implementation
+	createInstance:function(outer, iid) {
+		if (outer!=null) throw Components.results.NS_ERROR_NO_AGGREGATION;
+		return gFireFlvPolicy;
+	},
+
+	// nsISupports interface implementation
+	QueryInterface:function(iid) {
+		if (!iid.equals(Components.interfaces.nsISupports) &&
+			!iid.equals(Components.interfaces.nsISupportsWeakReference) &&
+			!iid.equals(Components.interfaces.nsIFactory) &&
+			!iid.equals(Components.interfaces.nsIObserver)
+		) {
+			throw Components.results.NS_ERROR_NO_INTERFACE;
+		}
+
+		return this;
+	}
+};
+
+// \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ //
+
+// Initialization and registration
+if ('undefined'==typeof(Components.classes[FIREFLV_POLICY__CONTRACTID])) {
+	(function() { // to keep from munging with scope
+		const CONTENTPOLICY_CONTRACTID="@mozilla.org/layout/content-policy;1";
+		const CONTENTPOLICY_DESCRIPTION="Content policy service";
+
+		// Component registration
+		var compMgr=Components.manager
+			.QueryInterface(Components.interfaces.nsIComponentRegistrar);
+		var cid=compMgr.contractIDToCID(CONTENTPOLICY_CONTRACTID);
+
+		compMgr.registerFactory(
+			cid, CONTENTPOLICY_DESCRIPTION, CONTENTPOLICY_CONTRACTID, gKablFactory
+		);
+		compMgr.registerFactory(
+			FIREFLV_POLICY__CID, CONTENTPOLICY_DESCRIPTION, FIREFLV_POLICY__CONTRACTID, gKablFactory
+		);
+	})();
+}
