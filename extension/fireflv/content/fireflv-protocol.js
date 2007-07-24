d56c7133389688c8428ed7aaeb323f5450a4f2a0
Anthony Lieuallen <arantius@gmail.com>
2007-07-24 03:52:32 +0000

    This approach doesn't work either!  Flash never requests the FLV.
    git-svn-id: http://svn.arantius.com@332 28f79fa8-d1f5-0310-81f3-f5ca3e6eca6b

--- extension/fireflv/content/fireflv-protocol.js
+++ extension/fireflv/content/fireflv-protocol.js
@@ -21,11 +21,21 @@ Protocol.prototype={
 	},
 
 	newChannel:function(aURI) {
+		dump('>>> FireFlv Protocol.newChannel()...\n');
+
 		// data:text/xml;charset=utf-8,%3C%3Fxml%20version%3D%221.0%22%3F%3E%3Ccross-domain-policy%3E%3Callow-access-from%20domain%3D%22*%22%2F%3E%3C%2Fcross-domain-policy%3E
 
+
 		var myUrl=aURI.spec;
-		dump('start request for ...\n'+myUrl+'\n');
-		myUrl=myUrl.replace('fireflv', 'http');
+		dump('FROM: '+myUrl+'\n');
+
+		var piece=myUrl.substring(18);
+		piece=piece.substring(0, piece.indexOf('?'));
+		if ('player'==piece) {
+			myUrl='chrome://fireflv/content/flvplayer.swf';
+		}
+		
+		dump('TO:   '+myUrl+'\n');
 
 		var ios=Components.classes[kIOSERVICE_CONTRACTID]
 			.getService(nsIIOService);
