2d9f92bd58068f8519d4666503498b5fdad1906e
Anthony Lieuallen <arantius@gmail.com>
2008-04-12 21:35:19 +0000

    * Version bump to 1.4.7 .  * Drop Firefox and Thunderbird 1.x support.  * Move app detection into chrome.manifest native support, drop custom code.
    git-svn-id: http://svn.arantius.com@359 28f79fa8-d1f5-0310-81f3-f5ca3e6eca6b

--- extension/tinymenu/content/tinymenu-options.js
+++ extension/tinymenu/content/tinymenu-options.js
@@ -1,38 +1,6 @@
 const FIREFOX_ID = "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}";
 const THUNDERBIRD_ID = "{3550f703-e582-4d05-9a08-453d09bdfdc6}";
 
-function loadOverlay(event) {
-	var appInfo=Components.classes["@mozilla.org/xre/app-info;1"]
-		.getService(Components.interfaces.nsIXULAppInfo);
-
-	if (FIREFOX_ID==appInfo.ID) {
-		// the label changed, so show the proper one
-		var versionChecker=Components
-			.classes["@mozilla.org/xpcom/version-comparator;1"]
-			.getService(Components.interfaces.nsIVersionComparator);
-		if (versionChecker.compare(appInfo.version, "2.0") >= 0) {
-			document.loadOverlay('chrome://tinymenu/content/tinymenu-options-ff2.xul', overlayObserver);
-		} else {
-			document.loadOverlay('chrome://tinymenu/content/tinymenu-options-ff1.xul', overlayObserver);
-		}
-
-		// the label changed, but not the ID, so always these IDs for firefox
-		tinymenu.menuIds=[
-			'file-menu', 'edit-menu', 'view-menu', 'go-menu',
-			'bookmarks-menu', 'tools-menu', 'helpMenu'
-		];
-	} else if (THUNDERBIRD_ID==appInfo.ID) {
-		document.loadOverlay('chrome://tinymenu/content/tinymenu-options-tb.xul', overlayObserver);
-
-		tinymenu.menuIds=[
-			'menu_File', 'menu_Edit', 'menu_View', 'menu_Go',
-			'messageMenu', 'tasksMenu', 'menu_Help'
-		];
-	} else {
-		// another app ??
-	}
-}
-
 function browseImage() {
 	// based on sample from
 	// http://developer.mozilla.org/en/docs/nsIFilePicker
@@ -78,7 +46,3 @@ var overlayObserver={
 		return this;
 	}
 };
-
-// calling at DOMContentLoaded instead of load has the convenient side effect
-// of avoiding the collision with CuteMenus, for me at least
-window.addEventListener('DOMContentLoaded', loadOverlay, true);
