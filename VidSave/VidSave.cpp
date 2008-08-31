d2a2d811a29829cb605166c15e014022c5aca4bf
Anthony Lieuallen <arantius@gmail.com>
2008-08-31 20:25:48 +0000

    space->tab
    git-svn-id: http://svn.arantius.com@433 28f79fa8-d1f5-0310-81f3-f5ca3e6eca6b

--- VidSave/VidSave.cpp
+++ VidSave/VidSave.cpp
@@ -2,63 +2,62 @@
 
 void PrintModules( DWORD processID )
 {
-    HMODULE hMods[1024];
-    HANDLE hProcess;
-    DWORD cbNeeded;
-    unsigned int i;
+	HMODULE hMods[1024];
+	HANDLE hProcess;
+	DWORD cbNeeded;
+	unsigned int i;
 
-    // Print the process identifier.
+	// Print the process identifier.
 
-    printf( "\nProcess ID: %u\n", processID );
+	printf( "\nProcess ID: %u\n", processID );
 
-    // Get a list of all the modules in this process.
+	// Get a list of all the modules in this process.
 
-    hProcess = OpenProcess( PROCESS_QUERY_INFORMATION |
-                            PROCESS_VM_READ,
-                            FALSE, processID );
-    if (NULL == hProcess)
-        return;
+	hProcess = OpenProcess( PROCESS_QUERY_INFORMATION |
+							PROCESS_VM_READ,
+							FALSE, processID );
+	if (NULL == hProcess)
+		return;
 
-    if( EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded))
-    {
-        for ( i = 0; i < (cbNeeded / sizeof(HMODULE)); i++ )
-        {
-            TCHAR szModName[MAX_PATH];
+	if( EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded))
+	{
+		for ( i = 0; i < (cbNeeded / sizeof(HMODULE)); i++ )
+		{
+			TCHAR szModName[MAX_PATH];
 
-            // Get the full path to the module's file.
+			// Get the full path to the module's file.
 
-            if ( GetModuleFileNameEx(hProcess, hMods[i], szModName,
-                                     sizeof(szModName)/sizeof(TCHAR)))
-            {
-                // Print the module name and handle value.
+			if ( GetModuleFileNameEx(hProcess, hMods[i], szModName,
+									 sizeof(szModName)/sizeof(TCHAR)))
+			{
+				// Print the module name and handle value.
 
-                _tprintf(TEXT("\t%s (0x%08X)\n"),
-                         szModName, hMods[i]);
-            }
-        }
-    }
+				_tprintf(TEXT("\t%s (0x%08X)\n"),
+						 szModName, hMods[i]);
+			}
+		}
+	}
 
-    CloseHandle( hProcess );
+	CloseHandle( hProcess );
 }
 
 void _tmain( )
 {
-    // Get the list of process identifiers.
+	// Get the list of process identifiers.
 
-    DWORD aProcesses[1024], cbNeeded, cProcesses;
-    unsigned int i;
+	DWORD aProcesses[1024], cbNeeded, cProcesses;
+	unsigned int i;
 
-    if ( !EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded ) )
-        return;
+	if ( !EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded ) )
+		return;
 
-    // Calculate how many process identifiers were returned.
+	// Calculate how many process identifiers were returned.
+	cProcesses = cbNeeded / sizeof(DWORD);
 
-    cProcesses = cbNeeded / sizeof(DWORD);
-
-    // Print the name of the modules for each process.
+	// Print the name of the modules for each process.
 
 	for ( i = 0; i < cProcesses; i++ ) {
-        PrintModules( aProcesses[i] );
+		PrintModules( aProcesses[i] );
 		_getch();
 	}
 }
