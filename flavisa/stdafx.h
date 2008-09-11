4a083c829778e36241ef3c6e9be9fc00669f3942
Anthony Lieuallen <arantius@gmail.com>
2008-09-11 00:03:43 +0000

    clean up unnecessary includes
    git-svn-id: http://svn.arantius.com@454 28f79fa8-d1f5-0310-81f3-f5ca3e6eca6b

--- flavisa/stdafx.h
+++ flavisa/stdafx.h
@@ -1,15 +1,15 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN
 
-#include <stdio.h>
 #include <tchar.h>
 #include <windows.h>
-#include <psapi.h>
-#include <strsafe.h>
-#include <shellapi.h>
-#include <commctrl.h>
-#include <time.h>
 
+#include <commctrl.h>
+#include <psapi.h>
+#include <shellapi.h>
+#include <strsafe.h> // For ErrorExit() .
+
+// For the Process class and its details.
 #using <System.dll>
 using namespace System;
 using namespace System::ComponentModel;
